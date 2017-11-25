c8d21a301d2555e28252460c5f54748029172c1d
Roshni Singh <RoSingh@art.com>
2017-11-26 01:28:28 +0530

    updated with HMR

--- src/main.js
+++ src/main.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Greeting from './Greeting';
+import { AppContainer } from 'react-hot-loader'
+import App from './components/App';
 
 ReactDOM.render(
-  <Greeting name={"Krishna"}/>,
+  <AppContainer>
+  <App name={"bujjii Akula"}/>
+  </AppContainer>,
   document.getElementById('root')
 );
+
+// Webpack Hot Module Replacement API
+if (module.hot) {
+  module.hot.accept('./components/App', () => { render(
+    <AppContainer>
+    <App name={"bujji Akula"}/>
+    </AppContainer>,
+    document.getElementById('root')
+  ) })
+}
