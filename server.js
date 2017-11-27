230006b602ab7cbbae940dee1c7ea9c776100f57
Ramakrishna Meda <rmeda@art.com>
2017-11-27 15:48:38 +0530

    Modified webpack and server js files

--- server.js
+++ server.js
@@ -7,14 +7,11 @@ var app = express();
 var compiler = webpack(config);
 
 app.use(require('webpack-dev-middleware')(compiler, {
+  noInfo: true,
   publicPath: config.output.publicPath
 }));
 
-app.use(require("webpack-hot-middleware")(compiler, {
-  log: false,
-  path: "/__webpack_hmr",
-  heartbeat: 20000
-}));
+app.use(require("webpack-hot-middleware")(compiler));
 
 app.get('*', function(req, res) {
   res.sendFile(path.join(__dirname, 'index.html'));
--- webpack.config.js
+++ webpack.config.js
@@ -4,38 +4,28 @@ var webpack = require('webpack');
 module.exports = {
     entry: [
         'react-hot-loader/patch',
-        'webpack-hot-middleware/client?path=/__webpack_hmr&timeout=20000&reload=true',
+        'webpack-hot-middleware/client',
         './src/main.js'
     ],
     output: {
+        path: path.join(__dirname, 'dist'),
         filename: 'bundle.js',
-        path: '/',
-        publicPath: 'http://localhost:3000/static/'
-    },
-    devServer: {
-        contentBase: './dist',
-        hot: true
-    },
-    resolve: {
-        extensions: ['', '.js', '.jsx']
+        publicPath: '/static/'
     },
     module: {
-        loaders: [{
+        loaders: [
+        {
             test: /\.js$/,
             exclude: /node_modules/,
             loaders: ['babel'],
             include: path.join(__dirname, 'src')
-          }]
+          }
+        ]
     },
     plugins: [
-        new webpack.optimize.OccurenceOrderPlugin(),
         new webpack.HotModuleReplacementPlugin(),
         new webpack.NoErrorsPlugin()
     ],
-    stats: {
-        // Nice colored output
-        colors: true
-    },
     // Create Sourcemaps for the bundle
     devtool: 'source-map',
 };
